7f52068ac58e68b96b99b114f580384d003010fd
Salem Hernandez <salem27@Salems-MacBook-Pro.local>
2026-03-04 14:07:02 -0800

    Create starting template for components

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+
+const HomePage = () => {
+
+  return (
+    <div className="my-component">
+      <h1>Hello World!</h1>
+      
+    </div>
+  );
+};
+
+export default HomePage;
