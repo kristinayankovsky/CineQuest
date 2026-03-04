00e63361c899ef1176f94d514f4997d7ad2fd8c3
Salem Hernandez <salem27@Salems-MacBook-Pro.local>
2026-03-04 14:18:46 -0800

    Create signup and login, add server file for cors use

---
+++ src/components/WatchList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+
+const WatchList = () => {
+
+  return (
+    <div className="my-component">
+      <h1>Hello World!</h1>
+      
+    </div>
+  );
+};
+
+export default WatchList;
