ed4954293cdaaa3e0b7e76eaeeda49ebb37ae377
Salem Hernandez <salem27@Salems-MacBook-Pro.local>
2026-03-04 14:31:53 -0800

    Add Login and signup components to app.jsx, created AuthToken component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import HomePage from './components/HomePage';
+import Profile from './components/Profile';
+import WatchList from './components/WatchList';
+import AuthToken from './components/AuthToken';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { token, setToken, logout } = AuthToken();
+
+  if(token){
+    return(
+      <>
+        <button onClick={logout} style={{float: 'right'}}>Logout</button>
+        <HomePage />
+      </>
+    );
+  }
 
   return (
     <>
-      
+      <section id='root'>
+        <h1>CineQuest!</h1>
+        <Login setToken={setToken}/>
+        
+        <SignUp setToken={setToken}/>
+      </section>
     </>
   )
 }
 
 export default App
+
