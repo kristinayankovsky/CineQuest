5ea1929b1b81290659b121d64bd58d1c107b53dd
nataly5pt <natalia.glushak@bellevuecollege.edu>
2026-03-23 18:22:30 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-// We will add Search and Profile imports here later!
+import MovieDetails from './pages/MovieDetails';
 
 function App() {
   return (
-    <div className="app">
-      {/* Navbar will go here later */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Other routes will go here */}
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<MovieDetails />} />
+    </Routes>
   );
 }
 
