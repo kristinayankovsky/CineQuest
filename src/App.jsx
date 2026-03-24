d948684df152531d95c11ad8d9f3448bbc95b86e
nataly5pt <natalia.glushak@bellevuecollege.edu>
2026-03-23 18:12:27 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,17 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import HomePage from "./components/HomePage";
-import Profile from "./components/Profile";
-import WatchList from "./components/WatchList";
-import MovieDetails from "./components/MovieDetails.jsx";
-import useAuthToken from "./components/useAuthToken";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// We will add Search and Profile imports here later!
 
 function App() {
-  const { token, setToken, logout } = useAuthToken();
-
-  // if we don’t have a token stored, show the login/signup form
-  if (!token) {
-    return (
-      <section className="auth">
-        <h1>CineQuest!</h1>
-        <Login setToken={setToken} />
-        <SignUp setToken={setToken} />
-      </section>
-    );
-  }
-
-  // otherwise render the normal application and expose a logout button
   return (
-    <>
-      <button onClick={logout} style={{ float: "right" }}>
-        Logout
-      </button>
-
+    <div className="app">
+      {/* Navbar will go here later */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/watchlist" element={<WatchList />} />
+        <Route path="/" element={<Home />} />
+        {/* Other routes will go here */}
       </Routes>
-    </>
+    </div>
   );
 }
 
